efe3500337066ffab42603517046ccc740bb7eae
mkq48 <152679213+mkq48@users.noreply.github.com>
2023-12-17 17:26:56 +0700

    Add files via upload

---
+++ BTTH03/BTTH03_1/btth03_1/config.php
@@ -0,0 +1,8 @@
+<?php
+// Database configuration
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'cms');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+?>
+ 
---
+++ BTTH03/BTTH03_2/Public/index.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="main">
+        <table>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Password</th>
+                <th>Created_at</th>
+                <th>Updated_at</th>
+            </tr>
+        </table>
+    </div>
+</body>
+</html>
